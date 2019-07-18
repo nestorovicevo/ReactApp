17501288d33459a614a23d4b2175e4f5e735eb10
nestorovicevo <nestorovicevo@gmail.com>
2019-07-18 12:23:33 +0200

    React Router, Links

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react' //ovo ce biti stateless komponenta, pa ne mora da se importuje {Component}
+import {Link} from 'react-router-dom';
 import'./Header.css'
 
 const Header = () => {
@@ -6,7 +7,9 @@ const Header = () => {
         // React.createElement('div', {className: 'react_div'}, 'This is a test')   ///ovo je komplikovaniji nacin, ali moze i ovako
         <div className="rmdb-header">
             <div className="rmdb-header-content">
-                <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+                <Link to="/">
+                    <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo"/>
+                </Link>
                 <img className="rmdb-tmdb-logo" src="./images/tmdb_logo.png" alt="tmdb-logo"/>
             </div>
         </div>
---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>
+                Whoooooops! This page doesn't exit!
+            </h1>
+        </div>
+    )
+}
+
+export default NotFound;
