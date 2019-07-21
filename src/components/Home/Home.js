b955125ebdcf47ae73c0be063ed864f3034a2fdd
nestorovicevo <nestorovicevo@gmail.com>
2019-07-21 23:31:14 +0200

    USE ASYNC AWAIT FOR FETCHING API DATA

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ class Home extends Component {
   componentDidMount() {
     if (localStorage.getItem('HomeState')) {
       const state = JSON.parse(localStorage.getItem('HomeState'));
-      this.setState({...state});
+      this.setState({ ...state });
     } else {
       this.setState({ loading: true });
       const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
@@ -57,28 +57,50 @@ class Home extends Component {
     this.fetchItems(endpoint);
   }
 
-  fetchItems = (endpoint) => {
-    fetch(endpoint)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({
-          movies: [...this.state.movies, ...result.results],
-          heroImage: this.state.heroImage || result.results[0],
-          loading: false,
-          currentPage: result.page,
-          totalPages: result.total_pages
-        }, () => {
-          if(this.state.searchTerm === ""){  //da nam ne bi cuvao localstorage ako smo pretrazivali neki pojam
-            localStorage.setItem('HomeState', JSON.stringify(this.state))
-          }
-        })
+  fetchItems = async endpoint => {
+    const { movies, heroImage, loading, currentPage, totalPages, searchTerm } = this.state;
+    const result = await (await fetch(endpoint)).json();
+    try {
+      this.setState({
+        movies: [...movies, ...result.results],
+        heroImage: heroImage || result.results[0],
+        loading: false,
+        currentPage: result.page,
+        totalPages: result.total_pages
+      }, () => {
+        if (searchTerm === "") {  //da nam ne bi cuvao localstorage ako smo pretrazivali neki pojam
+          localStorage.setItem('HomeState', JSON.stringify(this.state))
+        }
       })
-      .catch(error => console.error('Error:', error))
+    }
+    catch (e) {
+      console.log("There was an error: ", e)
+    }
   }
+
+  // fetchItems = (endpoint) => {
+  //   const { movies, heroImage, loading, currentPage, totalPages, searchTerm } = this.state;
+  //   fetch(endpoint)
+  //     .then(result => result.json())
+  //     .then(result => {
+  //       // this.setState({
+  //       //   movies: [...movies, ...result.results],
+  //       //   heroImage: heroImage || result.results[0],
+  //       //   loading: false,
+  //       //   currentPage: result.page,
+  //       //   totalPages: result.total_pages
+  //       // }, () => {
+  //       //   if (searchTerm === "") {  //da nam ne bi cuvao localstorage ako smo pretrazivali neki pojam
+  //       //     localStorage.setItem('HomeState', JSON.stringify(this.state))
+  //       //   }
+  //       // })
+  //     })
+  //     .catch(error => console.error('Error:', error))
+  // }
 
   render() {
     //destructuring the state
-    const {movies, heroImage, loading, currentPage, totalPages, searchTerm} = this.state;
+    const { movies, heroImage, loading, currentPage, totalPages, searchTerm } = this.state;
     return (
       <div className="rmdb-home">
         {heroImage ?
